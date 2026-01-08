3a53dacede642a224337aca2c3e57d324ecaf3b8
Developer Experience team at Ignite <hello@ignite.com>
2026-01-08 10:41:11 +0800

    Initialized with Ignite CLI

---
+++ x/postrachain/keeper/query_params_test.go
@@ -0,0 +1,22 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"postra-chain/x/postrachain/keeper"
+	"postra-chain/x/postrachain/types"
+)
+
+func TestParamsQuery(t *testing.T) {
+	f := initFixture(t)
+
+	qs := keeper.NewQueryServerImpl(f.keeper)
+	params := types.DefaultParams()
+	require.NoError(t, f.keeper.Params.Set(f.ctx, params))
+
+	response, err := qs.Params(f.ctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+}
